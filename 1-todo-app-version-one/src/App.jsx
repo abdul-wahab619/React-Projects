3420ef1eb6bccecf1813ed604b681d00da866de8
Abdul Wahab <awminhas619@gmail.com>
2023-12-15 17:32:57 +0500

    creating TodoItem2 component

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,6 +1,7 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import TodoItem1 from "./components/TodoItem2";
 
 function App() {
   return (
@@ -8,15 +9,7 @@ function App() {
       <AppName />
       <AddTodo />
       <TodoItem1 />
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-6">Go to College</div>
-          <div className="col-4">04/10/2023</div>
-          <div className="col-2">
-            <button className="btn btn-danger">Delete</button>
-          </div>
-        </div>
-      </div>
+      <TodoItem2 />
     </center>
   );
 }
---
+++ 1-todo-app-version-one/src/components/TodoItem2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TodoItem2 = () => {
+  let todoName = "Go to College";
+  let todoDate = "04/10/2023";
+  return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{todoDate}</div>
+        <div className="col-2">
+          <button className="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem2;
