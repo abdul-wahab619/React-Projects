e1845d7b63bd2390e295ab3e74faa04af73c3dec
Abdul Wahab <awminhas619@gmail.com>
2023-12-15 16:24:30 +0500

    simply creating UI of the app

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,5 +1,42 @@
 function App() {
-  return <>Placeholder</>;
+  return (
+    <center>
+      <h1>Todo App</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here..." />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button className="btn btn-success">Add</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">Buy Milk</div>
+          <div className="col-4">04/10/2023</div>
+          <div className="col-2">
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">Go to College</div>
+          <div className="col-4">04/10/2023</div>
+          <div className="col-2">
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
 export default App;
