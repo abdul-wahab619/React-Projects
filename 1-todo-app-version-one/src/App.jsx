b2a4afcda04d455f96c6efea94383bec196f23b0
Abdul Wahab <awminhas619@gmail.com>
2023-12-15 17:30:56 +0500

    create TodoItem1 component

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,22 +1,13 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
+import TodoItem1 from "./components/TodoItem1";
 
 function App() {
   return (
     <center>
       <AppName />
       <AddTodo />
-
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-6">Buy Milk</div>
-          <div className="col-4">04/10/2023</div>
-          <div className="col-2">
-            <button className="btn btn-danger">Delete</button>
-          </div>
-        </div>
-      </div>
-
+      <TodoItem1 />
       <div className="container text-center">
         <div className="row">
           <div className="col-6">Go to College</div>
