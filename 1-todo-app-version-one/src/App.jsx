4001d410add72ae2773ed2e3d9606e7746a9bfd4
Abdul Wahab <awminhas619@gmail.com>
2023-12-15 16:42:08 +0500

    create AddTodo component

--- 1-todo-app-version-one/src/App.jsx
+++ 1-todo-app-version-one/src/App.jsx
@@ -1,22 +1,11 @@
+import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
 
 function App() {
   return (
     <center>
       <AppName />
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-6">
-            <input type="text" placeholder="Enter Todo Here..." />
-          </div>
-          <div className="col-4">
-            <input type="date" />
-          </div>
-          <div className="col-2">
-            <button className="btn btn-success">Add</button>
-          </div>
-        </div>
-      </div>
+      <AddTodo />
 
       <div className="container text-center">
         <div className="row">
---
+++ 1-todo-app-version-one/src/components/AddTodo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const AddTodo = () => {
+  return (
+    <div>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter Todo Here..." />
+          </div>
+          <div className="col-4">
+            <input type="date" />
+          </div>
+          <div className="col-2">
+            <button className="btn btn-success">Add</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTodo;
