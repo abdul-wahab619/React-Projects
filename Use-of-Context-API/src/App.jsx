7be05a3ffbe5d2fca91c0c0f43871f0c7d5600b4
Abdul Wahab <awminhas619@gmail.com>
2023-12-23 23:30:10 +0500

    use useReducer to clean up the useState logic

--- Use-of-Context-API/src/App.jsx
+++ Use-of-Context-API/src/App.jsx
@@ -2,22 +2,49 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import TodoItemsContext from "./store/todo-items-store";
-
+const todoItemsReducer = (currState, action) => {
+  let newTodoItems = currState;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currState,
+      { name: action.payload.itemName, dueDate: action.payload.itemDueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currState.filter(
+      (item) => item.name != action.payload.itemName
+    );
+  }
+  return newTodoItems;
+};
 function App() {
-  const initialTodoItems = [];
-  const [todoItems, setTodoItems] = useState(initialTodoItems);
+  // const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
   const addNewItem = (itemName, itemDueDate) => {
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, dueDate: itemDueDate },
-    ]);
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
+
+    // setTodoItems((currValue) => [
+    //   ...currValue,
+    //   { name: itemName, dueDate: itemDueDate },
+    // ]);
   };
   const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name != todoItemName);
-    setTodoItems(newTodoItems);
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName: todoItemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction);
   };
   return (
     <TodoItemsContext.Provider
