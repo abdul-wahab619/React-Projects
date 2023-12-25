5d75544392631887dd6e88520603e75745a5368c
Abdul Wahab <awminhas619@gmail.com>
2023-12-25 22:33:03 +0500

    Data fetching using Fetch with the help of dummyjson api

---
+++ Advance-React/src/components/PostList.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+
+const PostList = () => {
+  const { postList, addInitalPosts } = useContext(PostListData);
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitalPosts(data.posts);
+      });
+  };
+  return (
+    <>
+      {postList.length === 0 && (
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+      )}
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostList;
