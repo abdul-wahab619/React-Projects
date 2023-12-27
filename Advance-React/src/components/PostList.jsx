923c578537e11ea769a27a21888941f001f6dcfb
Abdul Wahab <awminhas619@gmail.com>
2023-12-27 22:09:46 +0500

    handling loading state use loadingspinner

--- Advance-React/src/components/PostList.jsx
+++ Advance-React/src/components/PostList.jsx
@@ -4,22 +4,28 @@ import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 import { useState } from "react";
 import { useEffect } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitalPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
   useEffect(() => {
+    setFetching(true);
+    console.log("fetch started");
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitalPosts(data.posts);
+        setFetching(false);
+        console.log("fetch returned");
       });
+    console.log("fetch ended");
   }, []);
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {/*fetching &&*/ <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
